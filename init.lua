57ca13d951b9e7e4ace5533cdf4cdfdb1635afe6
Dragonop <Dragonop@users.noreply.github.com>
2016-03-16 23:57:48 -0300

    Mod
    code, textures, etc

---
+++ init.lua
@@ -0,0 +1,6 @@
+print("Obsidian tools")
+
+dofile(minetest.get_modpath("tools_obsidian").."/sword.lua")
+
+minetest.register_alias("sword_obsidian", "tools_obsidian:sword_obsidian")
+minetest.register_alias("longsword_obsidian", "tools_obsidian:longsword_obsidian")
